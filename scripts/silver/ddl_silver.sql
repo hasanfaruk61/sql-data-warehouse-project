bb953d4b7acc32677434598491a2d009f7dd1565
Hasan Faruk Pervan <84070821+hasanfaruk61@users.noreply.github.com>
2026-01-28 15:49:22 +0100

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,90 @@
+-- Creating Tables For Bronze Layer
+USE DataWarehouse;
+GO
 
+-- Checking if Table already exists
+DROP TABLE IF EXISTS  silver.crm_cust_info;
+GO
+-- Create CRM info☺
+CREATE TABLE silver.crm_cust_info(
+    cst_id                 INT,
+    cst_key                NVARCHAR(50),
+    cst_firstname          NVARCHAR(50),
+    cst_lastname           NVARCHAR(50),
+    cst_marital_status     NVARCHAR(50),
+    cst_gndr               NVARCHAR(50),
+    cst_create_date        DATE,
+    dwh_create_date        DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- Checking if Table already exists
+DROP TABLE IF EXISTS  silver.crm_prd_info;
+GO
+-- Create prd Info
+CREATE TABLE silver.crm_prd_info(
+    prd_id                 INT,
+    cat_id                 NVARCHAR(50),
+    prd_key                NVARCHAR(50),
+    prd_nm                 NVARCHAR(50),
+    prd_cost               INT,
+    prd_line               NVARCHAR(50),
+    prd_start_dt           DATE,
+    prd_end_dt             DATE,
+    dwh_create_date        DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- Checking if Table already exists
+DROP TABLE IF EXISTS  silver.crm_sales_details;
+GO
+-- Create Sales Details Info
+CREATE TABLE silver.crm_sales_details(
+    sls_ord_num             NVARCHAR(50),
+    sls_prd_key             NVARCHAR(50),
+    sls_cust_id             INT,
+    sls_ord_dt              DATE,
+    sls_ship_dt             DATE,
+    sls_due_dt              DATE,
+    sls_sales               INT,
+    sls_quantity            INT,
+    sls_price               INT,
+    dwh_create_date        DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- Checking if Table already exists
+DROP TABLE IF EXISTS  silver.erp_cust_az12;
+GO
+-- Create cust_az12 Table
+CREATE TABLE silver.erp_cust_az12(
+    cid         NVARCHAR(50),
+    bdate       DATE,
+    gndr        NVARCHAR(50),
+    dwh_create_date        DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- Checking if Table already exists
+DROP TABLE IF EXISTS  silver.erp_loc_a101;
+GO
+-- Create loc_a101 Table
+CREATE TABLE silver.erp_loc_a101(
+    cid        NVARCHAR(50),
+    cntry      NVARCHAR(50),
+    dwh_create_date        DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- Checking if Table already exists
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+GO
+--Create py_cat_g1v2 Table
+CREATE TABLE silver.erp_px_cat_g1v2(
+    id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50),
+    dwh_create_date        DATETIME2 DEFAULT GETDATE()
+);
+GO
